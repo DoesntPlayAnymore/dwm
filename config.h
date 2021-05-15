5425c7486bd58663bdc40bd249bc36371570d2f0
doesntplayanymore <cavasnel@gmail.com>
2021-05-15 14:45:00 +0200

    Failsafe.

--- config.h
+++ config.h
@@ -74,7 +74,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd  } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ ALTKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
-	{ ALTKEY,                       XK_Tab,    focusstack,     {.i = -1 } },
+	{ ALTKEY|ShiftMask,             XK_Tab,    focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_n,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
