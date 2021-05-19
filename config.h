a68ae673edbca209e59afb54668735b54484b28b
doesntplayanymore <cavasnel@gmail.com>
2021-05-20 01:03:48 +0200

    Pre actual fullscreen patch

--- config.h
+++ config.h
@@ -13,11 +13,11 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 13; 		/* window tile gap.*/
+static const unsigned int gappx     = 21; 		/* window tile gap.*/
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=13" };
-static const char dmenufont[]       = "monospace:size=13";
+static const char *fonts[]          = { "monospace:size=14" };
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -32,7 +32,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "W" };
+static const char *tags[] = { "Q", "W", "E", "A", "S", "D", "Z", "X", "C"} ;
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,8 +41,8 @@ static const Rule rules[] = {
 	 */
 	/* class                       instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",    		       NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox", 		       NULL,       NULL,       1 << 9,       0,           -1 },
-	{ "Firefox-developer-edition", NULL,       NULL,       1 << 9,       0,           -1 },
+	{ "Firefox", 		       NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox Developer Edition", NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -75,17 +75,18 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *pwdcmd[]   = { "cat", "~/.lastpwd", NULL };
 static const char *termcmd[]  = { "terminator", NULL };
 static const char *killslcmd[]= { "pkill", "slstatus", NULL };
-static const char *xblincrcmd[]   = { "xbacklight", "+10", NULL };
-static const char *xbldecrcmd[]   = { "xbacklight", "-10", NULL };
-static const char *xvoldecrcmd[]   = { "amixer", "sset", "master", "-10", NULL };
-static const char *xvolincrcmd[]   = { "amixer", "sset", "master", "+10", NULL };
-static const char *xvolmutecmd[]   = { "amixer", "sset", "master", "0", NULL };
+static const char *xblincrcmd[]   = { "xbacklight", "-inc", "+10", NULL };
+static const char *xbldecrcmd[]   = { "xbacklight", "-inc", "-10", NULL };
+static const char *xvoldecrcmd[]   = { "amixer", "sset", "Master", "66", NULL };
+static const char *xvolincrcmd[]   = { "amixer", "sset", "Master", "100", NULL };
+static const char *xvolmutecmd[]   = { "amixer", "sset", "Master", "0", NULL };
 static const char *surfcmd[]      = { "surf","duckduckgo.com", NULL };
+static const char *randwpcmd[]      = { "/home/pro/.scripts/randwp", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd  } },
+	{ MODKEY,                       XK_f,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_g,      spawn,          {.v = termcmd  } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ ALTKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ ALTKEY|ShiftMask,             XK_Tab,    focusstack,     {.i = -1 } },
@@ -93,11 +94,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_j,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_l,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_e,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_k,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_n,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -109,26 +110,26 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 
 	/* User defined. */
- 	{ MODKEY,                       XK_s,      spawn,          {.v = surfcmd     } },
-	{ 0,                            0x1008ff03,spawn,         {.v = xbldecrcmd } },
-	{ 0,                            0x1008ff02,spawn,         {.v = xblincrcmd } },
-	{ 0,                            0x1008ff11,spawn,         {.v = xvoldecrcmd }   },
-	{ 0,                            0x1008ff13,spawn,         {.v = xvolincrcmd }   },
-	{ 0,                            0x1008ff12,spawn,         {.v = xvolmutecmd }   },
-	{ 0,                            0xff61,    spawn_scrot,   {.v = "~/Pictures/scr/"} },
+ 	{ MODKEY,                       XK_v,      spawn,          {.v = surfcmd     }   },
+ 	{ MODKEY,                       XK_p,      spawn,          {.v = randwpcmd   }   },
+	{ 0,                            0x1008ff03,spawn,          {.v = xbldecrcmd  }   },
+	{ 0,                            0x1008ff02,spawn,          {.v = xblincrcmd  }   },
+	{ 0,                            0x1008ff11,spawn,          {.v = xvoldecrcmd }   },
+	{ 0,                            0x1008ff13,spawn,          {.v = xvolincrcmd }   },
+	{ 0,                            0x1008ff12,spawn,          {.v = xvolmutecmd }   },
+	{ 0,                            0xff61,    spawn_scrot,    {.v = "~/Pictures/scr/"} },
 
 	/* Tag keys. */
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	TAGKEYS(                        XK_w,                      9)
-  	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	TAGKEYS(                        XK_q,                      0)
+	TAGKEYS(                        XK_w,                      1)
+	TAGKEYS(                        XK_e,                      2)
+	TAGKEYS(                        XK_a,                      3)
+	TAGKEYS(                        XK_s,                      4)
+	TAGKEYS(                        XK_d,                      5)
+	TAGKEYS(                        XK_z,                      6)
+	TAGKEYS(                        XK_x,                      7)
+	TAGKEYS(                        XK_c,                      8)
+  	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
 /* button definitions */
